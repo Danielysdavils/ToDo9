0ff254b0fb6739fb48acedd4ce4f1c635c10dbc8
Danielys <danielisdavils@gmail.com>
2022-07-26 08:41:16 -0300

    "Ajustando arquivos e pastas"

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='container'>
+        <div className='top1'></div>
+        <div className='section1'>
+            <div className='indent'>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
